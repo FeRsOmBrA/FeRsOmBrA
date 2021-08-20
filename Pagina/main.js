331fef8c38872d2aa7c46ef877980fa64edd3b05
FeRsOmBrA <89277425+FeRsOmBrA@users.noreply.github.com>
2021-08-20 15:33:33 -0500

    Add files via upload

---
+++ Pagina/main.js
@@ -0,0 +1,9 @@
+document.querySelector('.menu-btn').addEventListener('click', () =>{
+    document.querySelector('.nav-menu').classList.toggle('show')
+});
+
+
+ScrollReveal().reveal('.homeproduct');
+ScrollReveal().reveal('.newreader' , {delay: 500});
+ScrollReveal().reveal('.banner1' , {delay: 500});
+ScrollReveal().reveal('.banner2' , {delay: 500});
