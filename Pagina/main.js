26a9f4dcd849ccdf6f499c6649626b113fa8e571
FeRsOmBrA <89277425+FeRsOmBrA@users.noreply.github.com>
2021-08-20 21:00:02 -0500

    Update main.js

--- Pagina/main.js
+++ Pagina/main.js
@@ -7,7 +7,7 @@ ScrollReveal().reveal('.homeproduct');
 ScrollReveal().reveal('.new1' , {delay: 500});
 ScrollReveal().reveal('.new2' , {delay: 500});
 ScrollReveal().reveal('.new3' , {delay: 500});
-ScrollReveal().reveal('.new4' , {delay: 500});
+ScrollReveal().reveal('.new5' , {delay: 500});
 ScrollReveal().reveal('.banner1' , {delay: 500});
 ScrollReveal().reveal('.banner2' , {delay: 500});
 ScrollReveal().reveal('.social' , {delay: 500});
